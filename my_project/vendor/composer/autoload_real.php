5c574128fb8a07e52de5feb668e15b1b91bde97b
daflpa <davidflorespadilla@outlook.es>
2016-02-23 19:38:20 +0000

    asdf1

--- my_project/vendor/composer/autoload_real.php
+++ my_project/vendor/composer/autoload_real.php
@@ -41,15 +41,19 @@ class ComposerAutoloaderInit3cd9631af1308420a041a6b08a888150
         $loader->register(true);
 
         $includeFiles = require __DIR__ . '/autoload_files.php';
-        foreach ($includeFiles as $file) {
-            composerRequire3cd9631af1308420a041a6b08a888150($file);
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire3cd9631af1308420a041a6b08a888150($fileIdentifier, $file);
         }
 
         return $loader;
     }
 }
 
-function composerRequire3cd9631af1308420a041a6b08a888150($file)
+function composerRequire3cd9631af1308420a041a6b08a888150($fileIdentifier, $file)
 {
-    require $file;
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+    }
 }
