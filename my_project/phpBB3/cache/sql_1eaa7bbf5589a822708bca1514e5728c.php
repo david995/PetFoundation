459e95bb6d659958145550a156afd518b6e57168
daflpa <davidflorespadilla@outlook.es>
2016-02-24 14:32:04 +0000

    bbdd

--- my_project/phpBB3/cache/sql_1eaa7bbf5589a822708bca1514e5728c.php
+++ my_project/phpBB3/cache/sql_1eaa7bbf5589a822708bca1514e5728c.php
@@ -1,5 +1,5 @@
 <?php exit; ?>
-1456224787
+1456314845
 SELECT forum_id FROM foro_forums WHERE forum_options & 2 <> 0 LIMIT 1
 6
 a:0:{}
--- my_project/phpBB3/cache/sql_e493eb7f48fe641b14a68db99c9394c3.php
+++ my_project/phpBB3/cache/sql_e493eb7f48fe641b14a68db99c9394c3.php
@@ -1,5 +1,5 @@
 <?php exit; ?>
-1456224487
+1456314545
 SELECT s.style_id, c.theme_id, c.theme_data, c.theme_path, c.theme_name, c.theme_mtime, i.*, t.template_path FROM foro_styles s, foro_styles_template t, foro_styles_theme c, foro_styles_imageset i WHERE s.style_id = 1 AND t.template_id = s.template_id AND c.theme_id = s.theme_id AND i.imageset_id = s.imageset_id
 71433
 a:1:{i:0;a:11:{s:8:"style_id";s:1:"1";s:8:"theme_id";s:1:"1";s:10:"theme_data";s:71046:"/*  phpBB3 Style Sheet
